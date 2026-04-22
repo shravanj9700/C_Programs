5732e528083fcd909877dfe1a35477b6e7d89b56
shravanj9700 <shravanj9700@gmail.com>
2026-04-23 00:45:10 +0530

    Add files via upload

---
+++ Shortlong.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int i = 11;
+  short int j = 11;
+  long int k = 11;
+
+  printf("Size of int : %lu\n",sizeof(i));           //4
+  printf("Size of short int : %lu\n",sizeof(j));     //2
+  printf("Size of long int : %lu\n",sizeof(k));      //8
+
+
+
+    return 0;
+}
