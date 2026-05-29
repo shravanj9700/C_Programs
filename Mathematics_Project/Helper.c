d53af28fd6d67f7318c4d2ce6b57511d61c2a7e3
shravanj9700 <shravanj9700@gmail.com>
2026-05-29 11:57:46 +0530

    Add files via upload

---
+++ Mathematics_Project/Helper.c
@@ -0,0 +1,11 @@
+//Function definition
+
+int Addition(int A, int B)
+{
+    return A+B;
+}
+
+int Substraction(int A, int B)
+{
+    return A-B;
+}
---
+++ Mathematics_Project/Marvellous.h
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+//Function Prototype / Function Declaration
+
+int Addition(int , int);
+
+int Substraction(int , int);
